dc91427e2280feac71146a48d62b6601d9ab6605
Paul Musco <paul_musco@hotmail.com>
2020-08-28 15:42:08 +0200

    Final repository tidying

--- combined_animals_table_code.sql
+++
@@ -1,6 +0,0 @@
-USE mammals_climate_change;
-CREATE TABLE combined_animals
-SELECT tam.Species, tam.CommonName_new, tam.Status, adp.Locality
-FROM table_awionline_mammals_csv tam
-	INNER JOIN animals_database_project_csv adp
-	ON tam.Species = adp.Species;
